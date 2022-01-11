1e934eb82b91d6eafc818c89e7e50a774870b076
seoyuun <carpedm1221@gmail.com>
2022-01-11 22:57:59 +0900

    고정형으로 우선 작업

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,24 +6,34 @@ const Navbar = () => {
     <div className="navbar">
       <nav className="navbar_container">
         <div className="navbar_dropmenu_logo">
-          <img
-            src="https://static.wanted.co.kr/images/icon-menu.png"
-            alt="hamberger menu"
-          />
-          <img
-            src="//theme.zdassets.com/theme_assets/9309779/480a35976bf401a88dd7388d8f5c19d77227cd35.png"
-            alt="원티드로고"
-            id="heder_logo_image"
-          />
+          <div className="navButton">
+            <img
+              src="https://static.wanted.co.kr/images/icon-menu.png"
+              alt="hamberger menu"
+            />
+          </div>
+          <div className="logoButton">
+            <img
+              src="//theme.zdassets.com/theme_assets/9309779/480a35976bf401a88dd7388d8f5c19d77227cd35.png"
+              alt="원티드로고"
+              id="heder_logo_image"
+            />
+          </div>
         </div>
         <ul className="navbar_main_menu">
           <li>채용</li>
           <li>이벤트</li>
           <li>직군별 연봉</li>
           <li>이력서</li>
-          <li>커뮤니티</li>
+          <li>
+            커뮤니티
+            <span>New</span>
+          </li>
           <li>프리랜서</li>
-          <li>AI 합격예측</li>
+          <li>
+            AI 합격예측
+            <span>Beta</span>
+          </li>
         </ul>
         <aside className="navbar_aside_menu">
           <ul>
@@ -31,7 +41,9 @@ const Navbar = () => {
               <FaSistrix className="search_icon" />
             </li>
             <li className="signButton">회원가입/로그인</li>
-            <li className="dashboardButton leftDevision">기업서비스</li>
+            <li className="leftDevision">
+              <span className="dashboardButton">기업 서비스</span>
+            </li>
           </ul>
         </aside>
       </nav>
