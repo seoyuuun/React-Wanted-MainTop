c11569bd7e428eabadf61b4906ab62b44263d6d5
seoyuun <carpedm1221@gmail.com>
2022-01-14 19:33:50 +0900

    Slide url로 이미지 불러오기 완료, 클릭 모션 아직 추가 안됨

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,16 +1,90 @@
-import React from "react";
-import SliderContent from "./SliderContent";
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
 import "./slider.scss";
+import SliderContent from "./SliderContent";
+import Slide from "./Slide";
+// import Arrow from "./Arrow";
 
-const Slider = () => {
+const Slider = (props) => {
+  const getWidth = () => window.innerWidth;
+
+  const [state, setState] = useState({
+    activeIndex: 0,
+    translate: 0,
+    transition: 0.45,
+  });
+
+  const { translate, transition, activeIndex } = state;
+
+  const nextSlide = () => {
+    if (activeIndex === props.slides.length - 1) {
+      return setState({
+        ...state,
+        translate: 0,
+        activeIndex: 0,
+      });
+    }
+
+    setState({
+      ...state,
+      activeIndex: activeIndex + 1,
+      translate: (activeIndex + 1) * getWidth(),
+    });
+  };
+
+  const prevSlide = () => {
+    if (activeIndex === 0) {
+      return setState({
+        ...state,
+        translate: (props.slides.length - 1) * getWidth(),
+        activeIndex: props.slides.length - 1,
+      });
+    }
+
+    setState({
+      ...state,
+      activeIndex: activeIndex - 1,
+      translate: (activeIndex - 1) * getWidth(),
+    });
+  };
+
   return (
-    <div
-      className="slider"
-      style={{ width: "100%", height: "100%", background: "#333" }}
-    >
-      <SliderContent />
-    </div>
+    <StyledSlider>
+      <SliderContent
+        translate={translate}
+        transition={transition}
+        width={getWidth() * props.slides.length}
+      >
+        {props.slides.map((slide, i) => (
+          <Slide key={slide + i} content={slide} />
+        ))}
+      </SliderContent>
+      <button
+        className="slider_arrow arrow_prev"
+        direction="left"
+        handleClick={prevSlide}
+      >
+        왼쪽
+      </button>
+      <button
+        className="slider_arrow arrow_next"
+        direction="right"
+        handleClick={nextSlide}
+      >
+        오른쪽
+      </button>
+      {/* <Arrow direction="left" handleClick={prevSlide} />
+      <Arrow direction="right" handleClick={nextSlide} /> */}
+    </StyledSlider>
   );
 };
+
+const StyledSlider = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  margin: 0 auto;
+  overflow: hidden;
+`;
 
 export default Slider;
--- src/components/slider/SliderContent.jsx
+++ src/components/slider/SliderContent.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import styled from "@emotion/styled";
+import styled from "styled-components";
 
 const SliderContent = styled.div`
   transform: translateX(-${(props) => props.translate}px);
   transition: transform ease-out ${(props) => props.transition}s;
-  height: 90%;
+  height: 100%;
   width: ${(props) => props.width}px;
   display: flex;
-  background: red;
 `;
 
 export default SliderContent;
