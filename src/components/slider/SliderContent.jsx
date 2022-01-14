e7e48f18953c2d8b3311fdc943d942672eb72be0
seoyuun <carpedm1221@gmail.com>
2022-01-14 20:02:58 +0900

    어긋나는 css 수정

--- src/components/slider/SliderContent.jsx
+++ src/components/slider/SliderContent.jsx
@@ -3,9 +3,10 @@ import styled from "styled-components";
 const SliderContent = styled.div`
   transform: translateX(-${(props) => props.translate}px);
   transition: transform ease-out ${(props) => props.transition}s;
-  height: 100%;
   width: ${(props) => props.width}px;
   display: flex;
+  margin: 50px 0;
+  position: relative;
 `;
 
 export default SliderContent;
