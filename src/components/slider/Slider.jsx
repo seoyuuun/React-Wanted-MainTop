c7f82091108ce20126604c25d471dbfefefddddf
seoyuun <carpedm1221@gmail.com>
2022-01-14 17:50:58 +0900

    슬라이드 이동 추가했으나 수동으로 list 추가 시  에러 발생

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./slider.scss";
 import SliderItems from "./slidItem/SliderItems";
 
 const Slider = () => {
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const sliderRef = useRef();
+
+  const hadleClick = (direction) => {
+    let distance = sliderRef.current.getBoundingClientRect().x;
+
+    console.log(distance);
+
+    if (direction === "left") {
+      setSlideNumber(slideNumber - 1);
+      sliderRef.current.style.transform = `translateX(${distance + 2000}px)`;
+    }
+    if (direction === "right") {
+      setSlideNumber(slideNumber + 1);
+      sliderRef.current.style.transform = `translateX(${distance - 210}px)`;
+    }
+  };
+
   return (
     <div className="slider">
-      <SliderItems />
-      <button className="slider_arrow arrow_prev">왼쪽</button>
-      <button className="slider_arrow arrow_next">오른쪽</button>
+      <div className="slider_container" ref={sliderRef}>
+        <SliderItems />
+      </div>
+      <button
+        className="slider_arrow arrow_prev"
+        onClick={() => hadleClick("left")}
+      >
+        왼쪽
+      </button>
+      <button
+        className="slider_arrow arrow_next"
+        onClick={() => hadleClick("right")}
+      >
+        오른쪽
+      </button>
     </div>
   );
 };
--- src/components/slider/slidItem/SliderItems.jsx
+++ src/components/slider/slidItem/SliderItems.jsx
@@ -1,3 +1,4 @@
+import React, { useRef } from "react";
 import "./sliderItems.scss";
 
 const SliderItems = () => {
